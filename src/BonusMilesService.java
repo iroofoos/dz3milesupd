53a51b0ddc24f6798f5636019010d7892269de55
koyotee <pere-noel@mail.ru>
2022-08-03 21:09:16 +0300

    reformat code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
 
-    public int calculate(int cost){
-int bonusMiles = cost / 20;
-return bonusMiles;
+    public int calculate(int cost) {
+        int bonusMiles = cost / 20;
+        return bonusMiles;
     }
 }
 
